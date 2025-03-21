4cb5f1bf5b6ffa6ac0d97a73731a3bb3b4e76f47
mobtaker61 <m.totonchi@hotmail.com>
2025-03-22 00:07:53 +0400

    s5-seed

--- database/seeders/MarketSeeder.php
+++ database/seeders/MarketSeeder.php
@@ -14,7 +14,9 @@ class MarketSeeder extends Seeder
             [
                 'id' => 1,
                 'name' => 'Nesto',
+                'local_name' => 'نيستو',
                 'logo' => 'market/CE8HGtLP84hRwq35L9rk4NN2GiQN8z3NrDg8NvF3.png',
+                'avatar' => 'markets/FfUzCkh85k6XHwnGMHrTe455cJ8XgVkg1JxCrhrb.jpg',
                 'website' => 'https://nestomakrket.com',
                 'android_app_link' => null,
                 'ios_app_link' => null,
@@ -25,7 +27,9 @@ class MarketSeeder extends Seeder
             [
                 'id' => 2,
                 'name' => 'Lu Lu Hypermarket',
+                'local_name' => 'لو لو هيبر ماركت',
                 'logo' => 'market/nEuCfTYR4QOyXFKB5ErwbE0xzfM5vHNgWObPjAQR.png',
+                'avatar' => 'markets/acZXLLfESdHjN1G0pAf4ErEbJfULqjfN4hjQDH4s.jpg',
                 'website' => 'https://luluhypermarket.com',
                 'android_app_link' => 'https://play.google.com/store/apps/details?id=com.akinon.lulushopping&pli=1',
                 'ios_app_link' => 'https://apps.apple.com/ae/app/lulu-hypermarket/id123456789',
@@ -36,7 +40,9 @@ class MarketSeeder extends Seeder
             [
                 'id' => 3,
                 'name' => 'Gift Villages',
+                'local_name' => 'جيفت فيليجز',
                 'logo' => 'market/wGlPbJYIogcLQfXLhoriCKk6zGcV17GHVG9kYkar.png',
+                'avatar' => 'markets/nTTYQA8DmqVe7dyoRNl9bcIdJdlo1jbTMlfjqhTh.jpg',
                 'website' => null,
                 'android_app_link' => null,
                 'ios_app_link' => null,
@@ -46,8 +52,10 @@ class MarketSeeder extends Seeder
             ],
             [
                 'id' => 4,
-                'name' => 'Day To Days',
+                'name' => 'Day To Day',
+                'local_name' => 'دي تو دي',
                 'logo' => 'market/va9yUjhDZ6rgErCH2DwyOtg44QhFCzY6sxkxUTRv.png',
+                'avatar' => 'markets/gJJwgJyjPZDQwMIrrJ2ksAUVcefXD8LYe9v3wVAH.jpg',
                 'website' => 'https://daytodayuae.com',
                 'android_app_link' => null,
                 'ios_app_link' => null,
@@ -58,7 +66,9 @@ class MarketSeeder extends Seeder
             [
                 'id' => 5,
                 'name' => 'Plus Royal Gift',
+                'local_name' => 'بلاس رويال جيفت',
                 'logo' => 'market/Fh67pVEQ7j9d4t94W788fMfZlDG69z7VlmocOcUW.png',
+                'avatar' => 'markets/qxnva8sNOCFCvE34OIK6hAzy04nn16pzoYEJrZCN.jpg',
                 'website' => 'https://plusgiftroyal.com',
                 'android_app_link' => null,
                 'ios_app_link' => null,
@@ -69,11 +79,26 @@ class MarketSeeder extends Seeder
             [
                 'id' => 6,
                 'name' => 'GALA',
+                'local_name' => 'جالا',
                 'logo' => 'market/1lciZmELaSte0OarottfPInTOl3ffiR35iMrIj1D.png',
+                'avatar' => 'markets/4laIroenWXnVhn7Tkrk5gfoPWcnV6JVt8zFVJpNX.jpg',
                 'website' => 'https://galamarkets.com',
                 'android_app_link' => null,
                 'ios_app_link' => null,
                 'whatsapp' => '+971562186121',
+                'created_at' => '2025-03-15 15:00:17',
+                'updated_at' => '2025-03-15 15:00:17'
+            ],
+            [
+                'id' => 7,
+                'name' => 'VIVA',
+                'local_name' => 'فيفا',
+                'logo' => 'market/RRAUc2UAamxt0Nig8VaHviGRE2t104JrhHex66sX.png',
+                'avatar' => 'markets/h4GyZyAO0rLG84uCidWixAmW2AMtmx1yyrSS90Gp.jpg',
+                'website' => 'https://myviva.com',
+                'android_app_link' => null,
+                'ios_app_link' => null,
+                'whatsapp' => null,
                 'created_at' => '2025-03-15 15:00:17',
                 'updated_at' => '2025-03-15 15:00:17'
             ]
